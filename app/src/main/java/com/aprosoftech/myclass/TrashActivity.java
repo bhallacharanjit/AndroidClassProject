33fbd21a4c21ca4e35aef1b856893abefc9e6333
CSB <bhallacharanjit@gmail.com>
2018-03-15 12:56:04 +0530

    scrollview and radio button

--- app/src/main/java/com/aprosoftech/myclass/TrashActivity.java
+++ app/src/main/java/com/aprosoftech/myclass/TrashActivity.java
@@ -2,12 +2,24 @@ package com.aprosoftech.myclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class TrashActivity extends AppCompatActivity {
+
+    RadioButton rb_male,rb_female;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trash);
+
+
+        rb_male = (RadioButton) findViewById(R.id.rb_male);
+        rb_female = (RadioButton) findViewById(R.id.rb_female);
+
+
+        if (rb_male.isChecked()) {
+
+        }
     }
 }
