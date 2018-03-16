f06e6f80d2cc65141748d4c920f2e6c1f2327b86
CSB <bhallacharanjit@gmail.com>
2018-03-16 11:58:40 +0530

    Custom Grid view

--- app/src/main/java/com/aprosoftech/myclass/GridAdapter.java
+++ app/src/main/java/com/aprosoftech/myclass/GridAdapter.java
@@ -1,31 +1,69 @@
 package com.aprosoftech.myclass;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by CSB on 15/03/18.
  */
 
 public class GridAdapter extends BaseAdapter {
+
+
+    ArrayList<HashMap<String, Object>> dataHashMap;
+    Context context;
+    LayoutInflater inflater;
+
+    public GridAdapter(Context context , ArrayList<HashMap<String,Object>> names) {
+        this.context = context;
+        this.dataHashMap = names;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+
     @Override
     public int getCount() {
-        return 0;
+        return this.dataHashMap.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return this.dataHashMap.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        View myView = view;
+        if (myView == null)
+            myView = inflater.inflate(R.layout.neta_dashboard_custom_grid,null);
+
+        TextView textView = (TextView) myView.findViewById(R.id.tv_title);
+        ImageView imageView = (ImageView) myView.findViewById(R.id.iv_icon);
+        RelativeLayout rl_inner_grid = (RelativeLayout) myView.findViewById(R.id.rl_inner_grid);
+
+        HashMap<String, Object> tempObj = this.dataHashMap.get(i);
+        textView.setText(tempObj.get("name").toString());
+        imageView.setImageResource((Integer) tempObj.get("image"));
+
+        rl_inner_grid.setBackgroundColor(Color.parseColor(tempObj.get("color").toString()));
+
+
+        return myView;
     }
 }
--- app/src/main/java/com/aprosoftech/myclass/GridViewActivity.java
+++ app/src/main/java/com/aprosoftech/myclass/GridViewActivity.java
@@ -1,15 +1,19 @@
 package com.aprosoftech.myclass;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GridViewActivity extends AppCompatActivity {
 
 
     GridView gv_dashboard;
-    String[] simpleArray = new String[]{"S1","S2","S3","S4","S5"};
+    String[] simpleArray = new String[]{"Add Neta","Edit Neta","View Neta","More Apps"};
+    Integer[] imagesArray= new Integer[]{R.mipmap.add_neta,R.mipmap.edit,R.mipmap.view,R.mipmap.more_apps};
+    String [] colorsArray = new String[]{"#2196F3","#9E9E9E","#4CAF50","#9C27B0"};
 
 
     @Override
@@ -20,8 +24,26 @@ public class GridViewActivity extends AppCompatActivity {
 
         gv_dashboard = (GridView) findViewById(R.id.gv_dashboard);
 
+//        ArrayAdapter arrayAdapter = new ArrayAdapter(GridViewActivity.this,android.R.layout.simple_list_item_1,simpleArray);
+//        gv_dashboard.setAdapter(arrayAdapter);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(GridViewActivity.this,android.R.layout.simple_list_item_1,simpleArray);
-        gv_dashboard.setAdapter(arrayAdapter);
+
+        ArrayList<HashMap<String,Object>> arrayList = new ArrayList<>();
+
+        for (int i=0;i<simpleArray.length;i++) {
+            HashMap<String, Object> tempHashmap = new HashMap<>();
+            tempHashmap.put("name",simpleArray[i]);
+            tempHashmap.put("image",imagesArray[i]);
+            tempHashmap.put("color",colorsArray[i]);
+
+
+            arrayList.add(tempHashmap);
+        }
+
+
+        GridAdapter adapter = new GridAdapter(GridViewActivity.this,arrayList);
+        gv_dashboard.setAdapter(adapter);
+
+
     }
 }
