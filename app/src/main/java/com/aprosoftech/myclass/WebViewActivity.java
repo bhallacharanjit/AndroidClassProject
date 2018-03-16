4ec776d13bbf801db49b697f215643846a9a0a5f
CSB <bhallacharanjit@gmail.com>
2018-03-16 12:42:30 +0530

    web view added

---
+++ app/src/main/java/com/aprosoftech/myclass/WebViewActivity.java
@@ -0,0 +1,48 @@
+package com.aprosoftech.myclass;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+
+        webView = (WebView) findViewById(R.id.wv_website);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.google.com");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+
+
+
+    class WebViewClient extends android.webkit.WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+//            webView.loadUrl(request.getUrl().getPath());
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Toast.makeText(WebViewActivity.this,"Page Loading...",Toast.LENGTH_LONG).show();
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            Toast.makeText(WebViewActivity.this,"Page Loaded...",Toast.LENGTH_LONG).show();
+            super.onPageFinished(view, url);
+        }
+    }
+}
